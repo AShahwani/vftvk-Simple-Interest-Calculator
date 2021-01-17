819e7531be5bdb54a1398670c37706f80fd4b83d
AShahwani <abshahwani@users.noreply.github.com>
2021-01-17 13:10:50 +0100

    First commit

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
+function calculate()
+    {
+        p = document.getElementById("principal").value;
+        n = document.getElementById("years").value;
+        r = document.getElementById("rate").value;
+        result = document.getElementById("result");
+        
+        result.innerHTML = "The interest is " + (p*n*r/100);
+    }
         
